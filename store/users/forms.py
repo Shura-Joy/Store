61e7597bedc6f0cb5a76e7fc445c40f2acda68ca
Александр Тимофеев <gagarart2@gmail.com>
2023-05-25 19:11:55 +0300

    better inputs

--- store/users/forms.py
+++ store/users/forms.py
@@ -1,9 +1,15 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from .models import User
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}), required=True)
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control py-4', 'placeholder': 'Введите пароль'}), required=True)
+
     class Meta:
         model = User
         fields = ('username', 'password')
