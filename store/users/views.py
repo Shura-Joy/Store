559f450a07e7a4d8066600da742be8545fa00a5d
Александр Тимофеев <gagarart2@gmail.com>
2023-05-25 15:35:45 +0300

    authorization logic

--- store/users/views.py
+++ store/users/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
+
+from .forms import UserLoginForm
 
 
 def login(request):
-    return render(request, "users/login.html")
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(
+                request, username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, "users/login.html", context)
 
 
 def register(request):
