b8ba202a2ce87e7b5c103fd4f5779e5db8f14ddf
Александр Тимофеев <gagarart2@gmail.com>
2023-06-03 18:40:59 +0300

    fix basket urls

--- store/products/urls.py
+++ store/products/urls.py
@@ -6,8 +6,8 @@ app_name = "products"
 
 urlpatterns = [
     path("", views.products, name="index"),
-    path("products/basket/add/<int:product_id>/",
+    path("basket/add/<int:product_id>/",
          views.basket_add, name="basket_add"),
-    path("products/basket/remove/<int:basket_id>/",
+    path("basket/remove/<int:basket_id>/",
          views.basket_remove, name="basket_remove"),
 ]
